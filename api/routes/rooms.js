d81adce67409591f3d17ec83479d6755a712e09e
it21156960 <chamith227@gmail.com>
2023-04-12 04:58:14 +0530

    t1

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,6 +1,24 @@
 import express from "express";
+import { createRoom, deleteRoom, getAllRoom, getRoom, updateRoom } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router =express.Router();
+//Create
+router.post("/:hotelid",verifyAdmin,createRoom)
+
+//update
+
+router.put("/:id",verifyAdmin,updateRoom)
+
+//Delete
+
+router.delete("/:id/:hotelid",verifyAdmin,deleteRoom)
+
+//get
+router.get("/:id", getRoom)
+
+// Get all hotels
+router.get("/",getAllRoom)
 
 
 export default router 
